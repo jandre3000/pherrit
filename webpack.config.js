dc678dde1df7fd8e6b87bdd0508776895c926931
Jan Drewniak <jan.drewniak@gmail.com>
2018-10-24 11:19:28 -0400

    Setting up for github pages
    - Moving dist files into 'docs' dir in order to serve this
    folder to github pages.
    - Creating a 'build' dir to house compiled dev assets.
    - Adding manifest.json to src dir and copying via webpack-copy-plugin.
    - Adding gh-pages url to manifest.json
    - Packaging .crx file using crx-webpack-plugin into 'docs' dir
    - Creating docs/index.html as public gh-pages landing page.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 const path = require( 'path' ),
 	MiniCssExtractPlugin = require( 'mini-css-extract-plugin' ),
-	CleanWebpackPlugin = require( 'clean-webpack-plugin' );
+	CleanWebpackPlugin = require( 'clean-webpack-plugin' ),
+	Crx = require( 'crx-webpack-plugin' ),
+	CopyWebpackPlugin = require( 'copy-webpack-plugin' );
 
 module.exports = {
 	mode: 'development',
 	entry: path.resolve( __dirname, 'src/main.js' ),
-
 	output: {
-		path: path.resolve( __dirname, 'build/chrome' ),
+		path: path.resolve( __dirname, 'build' ),
 		filename: 'main.js'
 	},
-
 	module: {
 		rules: [
 			{
@@ -19,11 +19,18 @@ module.exports = {
 			}
 		]
 	},
-
 	plugins: [
-		new CleanWebpackPlugin( 'dist', {} ),
+		new CleanWebpackPlugin( 'build' ),
+		new CopyWebpackPlugin( [ { from: 'src/manifest.json', to: 'manifest.json', toType: 'file' } ] ),
 		new MiniCssExtractPlugin( {
 			filename: 'style.css'
+		} ),
+		new Crx( {
+			keyFile: 'key.pem',
+			contentPath: 'build',
+			outputPath: 'docs',
+			name: 'pherrit-chrome-ext',
+			updateUrl: 'https://j4n-co.github.io/pherrit/'
 		} )
 	]
 
