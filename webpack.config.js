fa5c47497aa9b63080182dbb03868cb4f2151089
Jan Drewniak <jan.drewniak@gmail.com>
2018-10-24 12:37:58 -0400

    Removing gh-pages in favour of publishing to chrome store
    Removes crx packaging since chrome store just needs a zip of the build
    directory.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
 const path = require( 'path' ),
 	MiniCssExtractPlugin = require( 'mini-css-extract-plugin' ),
 	CleanWebpackPlugin = require( 'clean-webpack-plugin' ),
-	Crx = require( 'crx-webpack-plugin' ),
 	CopyWebpackPlugin = require( 'copy-webpack-plugin' );
 
 module.exports = {
-	mode: 'development',
+	mode: 'production',
 	entry: path.resolve( __dirname, 'src/main.js' ),
 	output: {
 		path: path.resolve( __dirname, 'build' ),
@@ -21,16 +20,12 @@ module.exports = {
 	},
 	plugins: [
 		new CleanWebpackPlugin( 'build' ),
-		new CopyWebpackPlugin( [ { from: 'src/manifest.json', to: 'manifest.json', toType: 'file' } ] ),
+		new CopyWebpackPlugin( [
+			{ from: 'src/manifest.json', to: 'manifest.json', toType: 'file' },
+			{ from: 'src/images/pherrit-logo.png', to: 'pherrit-logo.png', toType: 'file' }
+		] ),
 		new MiniCssExtractPlugin( {
 			filename: 'style.css'
-		} ),
-		new Crx( {
-			keyFile: 'key.pem',
-			contentPath: 'build',
-			outputPath: 'docs',
-			name: 'pherrit-chrome-ext',
-			updateUrl: 'https://j4n-co.github.io/pherrit/'
 		} )
 	]
 
